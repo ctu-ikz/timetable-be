d9cc165efd9c62be83f235404a2d4aaef74ab650
gax-il <dev@gaxil.eu>
2024-11-11 20:28:40 +0100

    Changes

--- controllers/CtrIndex.go
+++ controllers/CtrIndex.go
@@ -2,10 +2,9 @@ package controllers
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 )
 
 func GetIndex(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(fmt.Sprintf("Welcome to the timetable-be API \nVersion: 1.0"))
+	json.NewEncoder(w).Encode(map[string]string{"version": "1.0"})
 }
